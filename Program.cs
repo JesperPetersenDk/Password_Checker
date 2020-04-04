8fc4fc105e102def86664cf27e73da274d50b620
Jesper Petersen <info@ordklar.dk>
2020-04-04 23:05:23 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Password_Checker
 {
@@ -8,12 +9,14 @@ namespace Password_Checker
         {
             var plainTextPassword1 = "DF_SAskf2@_As!@?5546*%^__90";
 
-            for (var saltRounds = 10; saltRounds < 21; saltRounds++)
+            Stopwatch sw = Stopwatch.StartNew();
+
+            for (var saltRounds = 1; saltRounds < 100; saltRounds++)
             {
-                var dt = DateTime.Now;
-                Console.Write("START : Bcrype | cost: {0} - {1} - ",saltRounds, dt.Second);
+                Console.Write("START : Bcrype | cost: {0} - ",saltRounds);
                 var passwordHash = BCrypt.Net.BCrypt.HashPassword(plainTextPassword1);
-                Console.WriteLine("END: Bcrype | cost: {0} - {1} - PasswordHash: {2}", saltRounds, dt.Second, passwordHash);
+                Console.WriteLine("END: Bcrype | cost: {0} - Total tid: {1} - PasswordHash: {2}", saltRounds, sw.Elapsed.TotalMilliseconds, passwordHash);
+                sw.Stop();
             }
         }
     }
