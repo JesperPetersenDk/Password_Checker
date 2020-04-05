cb0df4a4729887eb0dfad31ae64d4bcaab749b55
Jesper Petersen <info@ordklar.dk>
2020-04-05 16:11:36 +0200

    Done

--- Program.cs
+++ Program.cs
@@ -8,15 +8,20 @@ namespace Password_Checker
         static void Main(string[] args)
         {
             var plainTextPassword1 = "DF_SAskf2@_As!@?5546*%^__90";
+            var plainTextPassword2 = "h=-&[`ZH5e*5QZ+JC&{?G&34_E^";
 
-            Stopwatch sw = Stopwatch.StartNew();
+            Stopwatch sw = new Stopwatch();
 
-            for (var saltRounds = 1; saltRounds < 100; saltRounds++)
+            var passwordx = BCrypt.Net.BCrypt.HashPassword(plainTextPassword2, 4);
+
+            for (var saltRounds = 4; saltRounds < 20; saltRounds++)
             {
-                Console.Write("START : Bcrype | cost: {0} - ",saltRounds);
-                var passwordHash = BCrypt.Net.BCrypt.HashPassword(plainTextPassword1);
-                Console.WriteLine("END: Bcrype | cost: {0} - Total tid: {1} - PasswordHash: {2}", saltRounds, sw.Elapsed.TotalMilliseconds, passwordHash);
+                sw.Start();
+                Console.Write("START : Bcrypt | cost: {0} - ", saltRounds);
+                var password = BCrypt.Net.BCrypt.HashPassword(plainTextPassword1, saltRounds);
+                Console.WriteLine("END: Bcrypt | cost: {0} - Total tid: {1} - PasswordHash: {2}", saltRounds, sw.Elapsed.TotalMilliseconds, password);
                 sw.Stop();
+                sw.Restart();
             }
         }
     }
